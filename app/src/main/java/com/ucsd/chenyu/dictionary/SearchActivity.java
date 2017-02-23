2e97633c731298d13f56dee2faa2b61de31f2591
Chenyu <chenyu19921103@gmail.com>
2017-02-23 15:31:27 -0800

    added auto pronouciation for words

--- app/src/main/java/com/ucsd/chenyu/dictionary/SearchActivity.java
+++ app/src/main/java/com/ucsd/chenyu/dictionary/SearchActivity.java
@@ -2,6 +2,7 @@ package com.ucsd.chenyu.dictionary;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
